9b96abf1796f1954b80f8168a12cd3cc919dd209
melonisj <melonisj@icloud.com>
2018-01-19 13:31:39 -0700

    added resume switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function resume_changer(){
-  var isChecked = $("#resume_checkbox").checked;
-  if(checked){
-    $("#resume_image").innerHTML = 	'<img src="../images/active-resume-ece.jpg" width="80%"/>';
+  var isChecked = document.getElementById("resume_checkbox").checked;
+  if(isChecked){
+    document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ece.jpg" width="80%"/>';
   } else {
-    $("#resume_image").innerHTML = 	'<img src="../images/active-resume-ast.jpg" width="80%"/>';
+    document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ast.jpg" width="80%"/>';
 
   }
 }
