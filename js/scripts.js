e288742690f24a04d0bdd488233472c936a59e1a
melonisj <melonisj@icloud.com>
2018-01-19 13:41:23 -0700

    fixed switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function resume_changer(){
   var isChecked = document.getElementById("resume_checkbox").checked;
-  if(isChecked){
+  if(!isChecked){
     document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ece.jpg" width="80%"/>';
   } else {
     document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ast.jpg" width="80%"/>';
