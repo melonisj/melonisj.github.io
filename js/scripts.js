722b66872a7ea581ddf6acd86ace1ca84b6c3ebd
melonisj <melonisj@icloud.com>
2018-01-21 21:47:20 -0700

    added detaislt o work history section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 function resume_changer(){
   var isChecked = document.getElementById("resume_checkbox").checked;
   if(!isChecked){
-    document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ece.jpg" width="80%"/>';
+    document.getElementById("resume_image").innerHTML = '<img src="../images/active-resume-ece.jpg" width="80%"/>';
+    document.getElementById("resume_jpg_download").innerHTML = '<a href="/active-resume-ece.pdf">Resume PDF'
+    document.getElementById("resume_pdf_download").innerHTML = '<a href="../images/active-resume-ece.jpg">Resume JPG'
+
   } else {
     document.getElementById("resume_image").innerHTML = 	'<img src="../images/active-resume-ast.jpg" width="80%"/>';
+    document.getElementById("resume_jpg_download").innerHTML = '<a href="/active-resume-ast.jpg">Resume PDF'
+    document.getElementById("resume_pdf_download").innerHTML = '<a href="../images/active-resume-ece.jpg">Resume JPG'
 
   }
 }
